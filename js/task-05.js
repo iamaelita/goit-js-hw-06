f53fd52890b7180fa276ba891b2c958bc545e73d
Aelita Deinychenko <amm-aelita@ukr.net>
2023-01-17 15:06:02 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -8,4 +8,12 @@
 const input = document.querySelector('#name-input');
 const output = document.querySelector('#name-output');
 
-function onInputTyping(event)
+function onInputTyping(event) {
+    output.textContent = event.currentTarget.value;
+
+    if (!output.textContent) {
+        output.textContent = 'Anonymous';
+    }
+}
+
+input.addEventListener('input', onInputTyping);
